ac09456618b9feacc1a430d77d7d7cceaa3bea4d
Nassgr <anas.grina@hotmail.fr>
2018-04-16 13:25:22 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder14
+ * Date: 16/04/18
+ * Time: 12:58
+ */
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Wcs\Hello;
+use HelloWorld\SayHello;
+
+
+$hello = new Hello();
+$helloWorld = new SayHello();
---
+++ src/Wcs/Hello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder14
+ * Date: 16/04/18
+ * Time: 12:58
+ */
+
+namespace App\Wcs;
+
+
+class Hello
+{
+    public function talk()
+    {
+       return "Hello World";
+    }
+}
